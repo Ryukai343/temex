b33292a64d7abb6033364c4abd51a646bf0dc620
Ryukai343 <Dorb343@gmail.com>
2024-01-21 10:00:03 +0100

    change in ajax for items_type show

--- public/js/ajax_items_type.js
+++ public/js/ajax_items_type.js
@@ -3,21 +3,7 @@ $(document).ready(function () {
     $('.ajax_type_mobile').click(function (e) {
         e.preventDefault();
         typeId = $(this).data('type-id');
-        alert(typeId);
-        $.ajax({
-            type: 'GET',
-            url: '/type/' + typeId,
-            data: {
-                // 'type_id': typeId,
-            },
-            success: function (data) {
-                // Replace the content of #item-container with the loaded items
-                $('#item-container').html(data);
-            },
-            error: function (data) {
-                console.error('Error loading items:', data);
-            }
-        });
+        callAjax(typeId);
     });
 });
 $(document).ready(function () {
@@ -25,17 +11,21 @@ $(document).ready(function () {
     $('.ajax_type').click(function (e) {
         e.preventDefault();
         typeId = $(this).data('type-id');
-        $.ajax({
-            type: 'GET',
-            url: '/type/' + typeId,
-            success: function (data) {
-                var itemContainerHtml = $('<div>').html(data).find('#item-container').html();
-                console.log(itemContainerHtml);
-                $('#item-container').html(itemContainerHtml);
-            },
-            error: function (data) {
-                console.error('Error loading items:', data);
-            }
-        });
+        callAjax(typeId);
     });
 });
+
+function callAjax(typeId) {
+    $.ajax({
+        type: 'GET',
+        url: '/type/' + typeId,
+        success: function (data) {
+            var itemContainerHtml = $('<div>').html(data).find('#item-container').html();
+            console.log(itemContainerHtml);
+            $('#item-container').html(itemContainerHtml);
+        },
+        error: function (data) {
+            console.error('Error loading items:', data);
+        }
+    });
+}
